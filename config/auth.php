46b7dad383948e008d7b13ff1516985b9b177a59
DunkaN <lguarnizl@gmail.com>
2020-12-28 23:43:54 -0500

    Se configuro laravel web socket y se probo artisan serve y artisan wesocket:serve en el mismo backend y funciona correcto

--- config/auth.php
+++ config/auth.php
@@ -43,7 +43,7 @@ return [
 
         'api' => [
             'driver' => 'passport',
-            'driver' => 'token',
+         //   'driver' => 'token',
             'provider' => 'users',
             'hash' => false,
         ],
