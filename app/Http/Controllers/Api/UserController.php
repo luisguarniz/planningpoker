d80ee3f1dfcd3ce94740af4933db9c2497230a00
DunkaN <lguarnizl@gmail.com>
2020-12-30 15:41:38 -0500

    se creo el metodo makeInvited en el controlador UserController.php para poder crear guardar y devolver el nuevo invitado

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -12,6 +12,7 @@ class UserController extends Controller
 {
 
     public $newUser;
+    public $newInvited;
     
     public function makeUser(){
         //Primero Creo un usuario
@@ -22,10 +23,10 @@ class UserController extends Controller
         $nomAnimal = $animal->animalName;
     
         $this->newUser = new User();
-        $this->newUser ->AdminUserCode = Uuid::uuid();
-        $this->newUser ->NameUsuario = $nomAnimal;
-        $this->newUser ->isAdmin = '1';
-        $this->newUser ->save();
+        $this->newUser->AdminUserCode = Uuid::uuid();
+        $this->newUser->NameUsuario = $nomAnimal;
+        $this->newUser->isAdmin = '1';
+        $this->newUser->save();
         
     //creamos un token que nos servira para la union de los usuarios
     $token = $this->newUser->CreateToken('authToken')->accessToken;
@@ -42,5 +43,23 @@ class UserController extends Controller
             'ok' => true,
             'user' => $this->newUser
         ]);
+    }
+
+    public function makeInvited(){
+
+
+        $animal = Animal::all()->random(); // con este metodo traigo un registro random
+        $nomAnimal = $animal->animalName;
+    
+        $this->newInvited = new User();
+        $this->newInvited->AdminUserCode = Uuid::uuid();
+        $this->newInvited->NameUsuario = $nomAnimal;
+        $this->newInvited->isInvited = '1';
+        $this->newInvited->save();
+        
+       $response['nameInvited']= $this->newInvited->NameUsuario;
+       $response ['InvitedUserCode'] = $this->newInvited->AdminUserCode ;
+        //devolvemos el codigo de usuario por que sera incertado en un campo al crear el room de la otra tabla
+        return $response;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+//Rutas del Host
 Route::get("User/makeUser",[UserController::class, 'makeUser'])->name('User.makeUser');
 Route::get('User/me',[UserController::class, 'me'])->name('UserController.me');
+
+//Rutas del Invited
+Route::get("User/makeInvited",[UserController::class, 'makeInvited'])->name('User.makeInvited');
+
+
+//Rutas del Room
 Route::post("Room/makeRoom",[RoomController::class, 'makeRoom'])->name('Room.makeRoom');
 //Route::get("Room/obtenerCiudad",[RoomController::class, 'obtenerCiudad'])->name('Room.obtenerCity');
 Route::put("Room/deactivateRoom",[RoomController::class, 'deactivateRoom'])->name('Room.deactivateRoom');
