2f978437636f734da6acf824763adc10abc50525
DunkaN <lguarnizl@gmail.com>
2021-01-13 23:48:22 -0500

    s

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -10,7 +10,7 @@ use Faker\Provider\Uuid;
 
 class RoomController extends Controller
 {
-    //este metodo espera el codigo y el token que devuelve la tabla user
+  //este metodo espera el codigo y el token que devuelve la tabla user
   public function makeRoom(Request $request)
   {
 
@@ -24,28 +24,29 @@ class RoomController extends Controller
     $roomCodeLetras = substr(str_shuffle($permitted_chars3), 0, 3);
     $permitted_chars4 = '0123456789';
     $roomCodeNumeros = substr(str_shuffle($permitted_chars4), 0, 3); //guardamos los caracteres aleatorios
-     
+
 
     $newRoom = new Room;
     $newRoom->RoomID = Uuid::uuid();
-    $newRoom->idAdmin = $request->idAdmin;// es lo que traigo en los parametros de la funcion
+    $newRoom->idAdmin = $request->idAdmin; // es lo que traigo en los parametros de la funcion
     $newRoom->RoomName = $roomName;
     $newRoom->RoomCode = $roomCodeLetras . $roomCodeNumeros;
     $newRoom->save();
 
 
     //una ves creada la sala con save() pasamos a retornar un objeto a la vista
-    $response['RoomName']= $newRoom->RoomName;
-    $response['RoomCode']= $newRoom->RoomCode;
-    $response['idAdmin'] = $newRoom->idAdmin;//este id admin que devuelve no es el que a creado la sala
+    $response['RoomName'] = $newRoom->RoomName;
+    $response['RoomCode'] = $newRoom->RoomCode;
+    $response['idAdmin'] = $newRoom->idAdmin; //este id admin que devuelve no es el que a creado la sala
     return $response;
   }
 
- 
-  public function desactivateRoom(Request $request){
-    
-          $room = Room::where('idAdmin',$request->idAdmin)->update([
-           'IsActive'=> '0'
-        ]);
+
+  public function desactivateRoom(Request $request)
+  {
+
+    $room = Room::where('idAdmin', $request->idAdmin)->update([
+      'IsActive' => '0'
+    ]);
   }
 }
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -13,97 +13,91 @@ use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
 
-    public $newUser;
-    public $newInvited;
-    
-    public function makeUser(){
-        //Primero Creo un usuario
-        $permitted_chars1 = '0123456789abcdefghijklmnopqrstuvwxyz';
-        $nrorandom =  substr(str_shuffle($permitted_chars1), 0, 4); //guardamos los caracteres aleatorios
+  public $newUser;
+  public $newInvited;
 
-        $animal = Animal::all()->random(); // con este metodo traigo un registro random
-        $nomAnimal = $animal->animalName;
-    
-        $this->newUser = new User();
-        $this->newUser->AdminUserCode = Uuid::uuid();
-        $this->newUser->NameUsuario = $nomAnimal.$nrorandom;
-        $this->newUser->password = bcrypt('12345678');
-        $this->newUser->isAdmin = '1';
-        $this->newUser->save();
-        
+  public function makeUser()
+  {
+    //Primero Creo un usuario
+    $permitted_chars1 = '0123456789abcdefghijklmnopqrstuvwxyz';
+    $nrorandom =  substr(str_shuffle($permitted_chars1), 0, 4); //guardamos los caracteres aleatorios
+
+    $animal = Animal::all()->random(); // con este metodo traigo un registro random
+    $nomAnimal = $animal->animalName;
+
+    $this->newUser = new User();
+    $this->newUser->AdminUserCode = Uuid::uuid();
+    $this->newUser->NameUsuario = $nomAnimal . $nrorandom;
+    $this->newUser->password = bcrypt('12345678');
+    $this->newUser->isAdmin = '1';
+    $this->newUser->save();
+
     //creamos un token que nos servira para la union de los usuarios
     $token = $this->newUser->CreateToken('authToken')->accessToken;
 
-  //  $response['NameUsuario']= $this->newUser->NameUsuario;
-   // $response ['AdminUserCode']= $this->newUser->AdminUserCode;
-  //  $response['isAdmin']= $this->newUser->isAdmin = '1';
-  //  $response ['token'] = $token;
-   //     return $response;
 
-       return response()->json([
-         'ok'  => true,
-        'user' => $this->newUser,
-         'token' => $token
-       ]);
-        }
 
-    public function loginHost(Request $request){
-      $data = $request->only('NameUsuario', 'password');
+    return response()->json([
+      'ok'  => true,
+      'user' => $this->newUser,
+      'token' => $token
+    ]);
+  }
 
-      if (!Auth::attempt($data)) {
-          return response()->json([
-           'ok'   => false,
-          'message' => 'error de credenciales',
-       ]);
-     }
+  public function loginHost(Request $request)
+  {
+    $data = $request->only('NameUsuario', 'password');
 
-      $token = Auth::user()->createToken('authToken')->accessToken;
-
+    if (!Auth::attempt($data)) {
       return response()->json([
-          'ok' => true,
-          'user' => Auth::user(),
-          'token' => $token
+        'ok'   => false,
+        'message' => 'error de credenciales',
       ]);
     }
 
+    $token = Auth::user()->createToken('authToken')->accessToken;
 
-    public function me(){
-        return response()->json([
-            'ok' => true,
-            'user' => Auth::user()
-        ]);
-    }
+    return response()->json([
+      'ok' => true,
+      'user' => Auth::user(),
+      'token' => $token
+    ]);
+  }
 
-    public function makeInvited(){
 
-        $permitted_chars1 = '0123456789abcdefghijklmnopqrstuvwxyz';
-        $nrorandom =  substr(str_shuffle($permitted_chars1), 0, 4); //guardamos los caracteres aleatorios
+  public function me()
+  {
+    return response()->json([
+      'ok' => true,
+      'user' => Auth::user()
+    ]);
+  }
 
-        $animal = Animal::all()->random(); // con este metodo traigo un registro random
-        $nomAnimal = $animal->animalName;
-    
-        $this->newInvited = new User();
-        $this->newInvited->AdminUserCode = Uuid::uuid();
-        $this->newInvited->NameUsuario = $nomAnimal.$nrorandom;
-        $this->newInvited->password = bcrypt('12345678');
-        $this->newInvited->isInvited = '1';
-        $this->newInvited->save();
-        
+  public function makeInvited()
+  {
 
-        //creamos un token que nos servira para la union de los usuarios
-      $token = $this->newInvited->CreateToken('authToken')->accessToken;
+    $permitted_chars1 = '0123456789abcdefghijklmnopqrstuvwxyz';
+    $nrorandom =  substr(str_shuffle($permitted_chars1), 0, 4); //guardamos los caracteres aleatorios
 
-//       $response['NameUsuario']= $this->newInvited->NameUsuario;
-//       $response['AdminUserCode']= $this->newInvited->AdminUserCode;
- //      $response['isInvited']= $this->newInvited->isInvited = '1';
- //      $response ['token'] = $token;
-        //devolvemos el codigo de usuario por que sera incertado en un campo al crear el room de la otra tabla
- //       return $response;
- return response()->json([
-    'ok'  => true,
-   'user' => $this->newInvited,
-    'token' => $token
-  ]);
+    $animal = Animal::all()->random(); // con este metodo traigo un registro random
+    $nomAnimal = $animal->animalName;
 
-    }
+    $this->newInvited = new User();
+    $this->newInvited->AdminUserCode = Uuid::uuid();
+    $this->newInvited->NameUsuario = $nomAnimal . $nrorandom;
+    $this->newInvited->password = bcrypt('12345678');
+    $this->newInvited->isInvited = '1';
+    $this->newInvited->save();
+
+
+    //creamos un token que nos servira para la union de los usuarios
+    $token = $this->newInvited->CreateToken('authToken')->accessToken;
+
+
+    return response()->json([
+      'ok'  => true,
+      'user' => $this->newInvited,
+      'token' => $token
+    ]);
+  }
 }
