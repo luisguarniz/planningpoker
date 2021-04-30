f3a81181abfe05bbc1fd62537f9f5f857c9f5aa1
DunkaN <lguarnizl@gmail.com>
2021-04-29 21:23:11 -0500

    se creo un metodo especial para traer la informacion que sera repartida en la vista apartir del componente host-inicio quitando los dataservice que estan involucrados y en su lugar cambiando por inputs de componente padre hacia hijo

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -69,6 +69,23 @@ class RoomController extends Controller
     ]);
   }
 
+  public function getRoomhost(Request $request)
+  {
+    $message = null;
+    $room = Room::where('RoomCode',$request->RoomCode)->first();
+
+    if ($room == null) {
+      return $message;
+    }
+    return response()->json([
+
+      'RoomID'=>$room->RoomID,
+      'RoomName' => $room->RoomName,
+      'RoomCode' => $room->RoomCode,
+      'idAdmin'=> $room->idAdmin
+    ]);
+  }
+
   public function makeStatus(Request $request)
   {
       $this->newStatu = new Statu();
