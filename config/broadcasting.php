2d1eeb5565a9e13c6e5003a419f2bd35424ee8f9
DunkaN <lguarnizl@gmail.com>
2021-08-10 23:07:38 -0500

    dejando la configuracion como cuando funcionaba con http

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,12 +38,9 @@ return [
             'options' => [
                 'useTLS' => true,
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-               // 'host' => '127.0.0.1',
-                'host' => 'api.agilefacilitationnetwork.com',
+                'host' => '127.0.0.1',
                 'port' => 6001,
-                //'scheme' => 'http'
-                'scheme' => 'https'
+                'scheme' => 'http'
             ],
         ],
 
--- config/websockets.php
+++ config/websockets.php
@@ -116,14 +116,14 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-       // 'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
-        'local_cert' => '/etc/letsencrypt/live/api.agilefacilitationnetwork.com/fullchain.pem',
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-       // 'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
-       'local_pk' => '/etc/letsencrypt/live/api.agilefacilitationnetwork.com/privkey.pem',
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+
         /*
          * Passphrase for your local_cert file.
          */
